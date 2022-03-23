bc2220bdeca4a476a438bad9a3305e5089c4afa1
Adrian <niedziolkadadrian@gmail.com>
2022-03-23 17:50:56 +0100

    StartProjekt with gitignore

---
+++ Mathio/Mathio/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mathio.Models;
+
+namespace Mathio.Controllers;
+
+public class ProfileController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+}
