fc4fe6f4dfd3bc189d520e7f849139b621cb59ee
Rafal <rniedziolkad@gmail.com>
2022-03-28 16:23:16 +0100

    Obsluga logowania

--- Mathio/Mathio/Controllers/ProfileController.cs
+++ Mathio/Mathio/Controllers/ProfileController.cs
@@ -1,11 +1,19 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Mathio.Models;
+using Firebase.Auth;
 
 namespace Mathio.Controllers;
 
 public class ProfileController : Controller
 {
+    private FirebaseAuthProvider _auth;
+
+    public ProfileController()
+    {
+        _auth = new FirebaseAuthProvider(
+            new FirebaseConfig("AIzaSyAFjhO8zLz4S-nUoZyEtXZbzawQ0oor78k"));
+    }
     // GET
     public IActionResult Index()
     {
@@ -16,12 +24,36 @@ public class ProfileController : Controller
     {
         return View();
     }
-    public Task<IActionResult> Register(User user){
-        return None;
-    }
+    
     public IActionResult SignIn()
     {
         return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> SignIn(UserModel userModel)
+    {
+        //log in the new user
+        var fbAuth = await _auth
+            .SignInWithEmailAndPasswordAsync(userModel.Email, userModel.Password);
+        await fbAuth.RefreshUserDetails();
+        string token = fbAuth.FirebaseToken;
+        //saving the token in a session variable
+        if (token != null && fbAuth.User.IsEmailVerified)
+        {
+            HttpContext.Session.SetString("_UserToken", token);
+
+            return RedirectToAction("Index");
+        }
+        else if(!fbAuth.User.IsEmailVerified)
+        {
+            ViewBag.Reason = "Login failed: Email Not Verified!";
+            return View();
+        }
+        else
+        {
+            ViewBag.Reason = "Login failed!";
+            return View();
+        }
     }
     
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
