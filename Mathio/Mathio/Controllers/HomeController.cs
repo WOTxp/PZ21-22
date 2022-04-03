b276d6ac3a8113b43b9fb166f97b0aeb80fb279f
Adrian <niedziolkadadrian@gmail.com>
2022-04-03 15:55:52 +0200

    Databse connection and tests

--- Mathio/Mathio/Controllers/HomeController.cs
+++ Mathio/Mathio/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Mathio.Models;
+using Google.Cloud.Firestore;
 
 namespace Mathio.Controllers;
 
@@ -21,6 +22,30 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+    
+    public string Databasetest()
+    {
+        FirestoreDb db = FirestoreDb.Create("pz202122-cf12f");
+        return "Success";
+    }
+
+    
+    public async Task<string> Databasegettasks()
+    {
+        string ret = "";
+        FirestoreDb db = FirestoreDb.Create("pz202122-cf12f");
+        
+        CollectionReference usersRef = db.Collection("Tasks");
+        QuerySnapshot snapshot = await usersRef.GetSnapshotAsync();
+        foreach (DocumentSnapshot doc in snapshot.Documents)
+        {
+            ret += string.Format("Document Id: {0}\n", doc.Id);
+            Dictionary<string, object> docDict = doc.ToDictionary();
+            ret += string.Join(Environment.NewLine, docDict);
+        }
+
+        return ret;
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
