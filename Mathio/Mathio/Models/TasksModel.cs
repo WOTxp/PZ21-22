cab57925e3f67b55e3aa4f41415e7ba7f18774c5
Adrian <niedziolkadadrian@gmail.com>
2022-04-09 18:08:38 +0200

    Fixing models

--- Mathio/Mathio/Models/TasksModel.cs
+++ Mathio/Mathio/Models/TasksModel.cs
@@ -1,13 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Google.Cloud.Firestore;
 
 namespace Mathio.Models;
 
+[FirestoreData]
 public class TasksModel
 {
+    [FirestoreDocumentId]
     public string ID { get; set; }
     [Required]
+    [FirestoreDocumentId]
     public string Author { get; set; }
     [Required]
+    [FirestoreProperty]
     public string Title { get; set; }
+    [FirestoreProperty]
     public string Description { get; set; }
 }
