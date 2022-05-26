35af70a654a7865b092aef3e3a778551a4fcd00e
Adrian <niedziolkadadrian@gmail.com>
2022-05-26 10:45:24 +0200

    Edit and Delete Start

--- Mathio/Mathio/Controllers/TasksManagerController.cs
+++ Mathio/Mathio/Controllers/TasksManagerController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Firebase.Auth;
 using Microsoft.AspNetCore.Mvc;
 using Mathio.Models;
 using Google.Cloud.Firestore;
@@ -10,14 +11,19 @@ namespace Mathio.Controllers;
 
 public class TasksManager : Controller
 {
+    private FirebaseAuthProvider _auth;
     private FirestoreDb _db;
+    private static DocumentSnapshot? _lastDoc;
     public TasksManager()
     {
+        _auth = new FirebaseAuthProvider(
+            new FirebaseConfig("AIzaSyAFjhO8zLz4S-nUoZyEtXZbzawQ0oor78k"));
         _db = FirestoreDb.Create("pz202122-cf12f");
     }
     // GET
     public IActionResult Index()
     {
+        _lastDoc = null;
         return View();
     }
     
@@ -25,7 +31,47 @@ public class TasksManager : Controller
     {
         return View();
     }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddTask([Bind("Task, Lessons, Questions")] TasksAllModel newTask)
+    {
+        if (ModelState.IsValid)
+        {
+            CollectionReference tasks = _db.Collection("Tasks");
+            DocumentReference task = await tasks.AddAsync(newTask.Task);
+            
+            CollectionReference lessons = task.Collection("Lessons");
+            foreach (LessonModel l in newTask.Lessons)
+            {
+                await lessons.AddAsync(l);
+            }
 
+            CollectionReference questions = task.Collection("Questions");
+            foreach (QuestionModel q in newTask.Questions)
+            {
+                await questions.AddAsync(q);
+            }
+            
+            return RedirectToAction("Index");
+        }
+        return View(newTask);
+    }
+    
+    public async Task<IActionResult> Delete(string id="")
+    {
+        if (id == "")
+            return RedirectToAction("Index", "TasksManager");
+        DocumentReference task = _db.Collection("Tasks").Document(path: id);
+        TasksModel t = task.GetSnapshotAsync().Result.ConvertTo<TasksModel>();
+        return View(t);
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> Delete(string id = "")
+    {
+        return RedirectToAction("Index", "TasksManager");
+    }
+    
     public async Task<IActionResult> EditTask(string id="")
     {
         if (id == "")
@@ -57,33 +103,17 @@ public class TasksManager : Controller
         Console.WriteLine(t.Title+" "+t.ID);
         return View(tall);
     }
-    
+
     [HttpPost]
-    public async Task<IActionResult> AddTask([Bind("Task, Lessons, Questions")] TasksAllModel newTask)
+    public async Task<IActionResult> EditTask(TasksAllModel task)
     {
         if (ModelState.IsValid)
         {
-            CollectionReference tasks = _db.Collection("Tasks");
-            DocumentReference task = await tasks.AddAsync(newTask.Task);
-            
-            CollectionReference lessons = task.Collection("Lessons");
-            foreach (LessonModel l in newTask.Lessons)
-            {
-                await lessons.AddAsync(l);
-            }
-
-            CollectionReference questions = task.Collection("Questions");
-            foreach (QuestionModel q in newTask.Questions)
-            {
-                await questions.AddAsync(q);
-            }
-            
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", "TasksManager");
         }
-        return View(newTask);
+        return View(task);
     }
-    
-    
+
     [HttpPost]
     public IActionResult AddLesson([Bind("Lessons")] TasksAllModel m)
     {
@@ -97,6 +127,33 @@ public class TasksManager : Controller
         m.Questions.Add(new QuestionModel());
         Console.WriteLine(m.Questions.Count);
         return PartialView("TasksManager/_QuestionsList", m);
+    }
+    
+    public async Task<IActionResult> LoadMoreTasksM(int batchSize = 2)
+    {
+        string? token = HttpContext.Session.GetString("_UserToken");
+        if (!String.IsNullOrEmpty(token))
+        {
+            var UserId = _auth.GetUserAsync(token).Result.LocalId;
+            DocumentReference author = _db.Collection("Users").Document(UserId);
+            Query tasksQuery = _db.Collection("Tasks").WhereEqualTo("Author", author).OrderBy("Category");
+            if (_lastDoc != null)
+            {
+                tasksQuery = tasksQuery.StartAfter(_lastDoc);
+            }
+            tasksQuery = tasksQuery.Limit(batchSize);
+            
+            QuerySnapshot snapshot = await tasksQuery.GetSnapshotAsync();
+            List<TasksModel> tasks = new List<TasksModel>();
+            foreach (DocumentSnapshot document in snapshot.Documents)
+            {
+                tasks.Add(document.ConvertTo<TasksModel>());
+            }
+
+            _lastDoc = snapshot.Documents.Count > 0 ? snapshot.Documents.Last() : _lastDoc;
+            return PartialView("TasksManager/_TasksMBatch", tasks);
+        }
+        return PartialView("TasksManager/_TasksMBatch", new List<TasksModel>());
     }
     
     
