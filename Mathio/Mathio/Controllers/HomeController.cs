a670557980580d00c6535d04605125c0866dbdfc
Adrian <niedziolkadadrian@gmail.com>
2022-06-18 18:24:53 +0200

    Do logowania

--- Mathio/Mathio/Controllers/HomeController.cs
+++ Mathio/Mathio/Controllers/HomeController.cs
@@ -1,23 +1,14 @@
 ﻿using System.Diagnostics;
-using Firebase.Auth;
 using Microsoft.AspNetCore.Mvc;
 using Mathio.Models;
-using Google.Cloud.Firestore;
+
+
 
 namespace Mathio.Controllers;
 
 public class HomeController : Controller
 {
-    private FirebaseAuthProvider _auth;
-    private FirestoreDb _db;
-
-    public HomeController()
-    {
-        _auth = new FirebaseAuthProvider(
-            new FirebaseConfig("AIzaSyAFjhO8zLz4S-nUoZyEtXZbzawQ0oor78k"));
-        _db = FirestoreDb.Create("pz202122-cf12f");
-    }
-
+    
     public IActionResult Index()
     {
         var token = HttpContext.Session.GetString("_UserToken");
@@ -30,7 +21,6 @@ public class HomeController : Controller
     {
         var token = HttpContext.Session.GetString("_UserToken");
         ViewBag.Layout = string.IsNullOrEmpty(token) ? "_Layout2" : "_Layout";
-        
         return View();
     }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
